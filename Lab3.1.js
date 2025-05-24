f80f6799a67e60379fab86a2524aae1c6a37a9cc
PestArzt <72229796+PestArzt1936@users.noreply.github.com>
2025-05-25 01:47:16 +0300

    Added additional files for my lab work

---
+++ Lab3.1.js
@@ -0,0 +1,12 @@
+function calcD(a, b, c) {
+return b*b - 4*a*c;
+}
+function Equal(a,b){
+    if(a==b)
+    return true;
+    else
+    return false;
+}
+console.log(calcD(2,12,24));
+console.log(Equal(5,5));
+console.log(Equal(3,5));
